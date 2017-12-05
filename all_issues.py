1c12d3af98281514e286495f12f4ad56e050c03d
Ben <benscarboro@hotmail.com>
2017-12-05 18:32:05 +0000

    Added download for Hackspace magazine
    Change in code to make a list of the Hackspace magazines to download as well as MagPi magazines

--- all_issues.py
+++ all_issues.py
@@ -8,9 +8,28 @@ debug = False
 
 # specify the urlof the MagPi page, with the "Download Free" button
 base_url = 'https://www.raspberrypi.org/magpi/issues/'
+base_anchor = '<a href=\"(?P<link>.+?pdf)\".+?>Download Free<\/a>'
+base2_url = 'https://hackspace.raspberrypi.org/issues/'
+base2_anchor = 'href=\"(?P<link>.+?\.pdf\?[0-9]{0,20})\">Download free PDF<\/a>'
 # Place to put the files (Yes, I developed it on windows!)
 # CHANGE THIS FOR YOUR OWN PATH
 output_dir = "D:\\MagPi\\"
+
+
+def GetLinks(url, regEx):
+	if debug:
+		print("Downloading " + url)
+	# query the website and return the html to the variable 'page'
+	response = urllib2.urlopen(url)
+	page = response.read()
+	response.close()
+
+	if debug:
+		print("Now doing a RegEx on the page text with regex='" + regEx + "'")
+		print(str(page))
+
+	# Go through the page and find all the "Download Free" links
+	return re.findall(regEx, str(page), flags=re.IGNORECASE)
 
 
 # Path to MagPi Magazines
@@ -19,6 +38,8 @@ magpi_path = os.path.join(output_dir, "Magazines")
 essen_path = os.path.join(output_dir, "Essentials")
 # Path to Other Magazines
 other_path = os.path.join(output_dir, "Other")
+# HackSpace magazine
+hs_path = os.path.join(output_dir, "HackSpace")
 
 # Make sure the output folders is available
 if not os.path.exists(magpi_path):
@@ -30,20 +51,13 @@ if not os.path.exists(essen_path):
 if not os.path.exists(other_path):
 	print("Creating '{0}' now".format(other_path))
 	os.makedirs(other_path)
+if not os.path.exists(hs_path):
+	print("Creating '{0}' now".format(hs_path))
+	os.makedirs(hs_path)
 
-if debug:
-	print("Downloading " + base_url)
+links = GetLinks(base_url, base_anchor)
+links += GetLinks(base2_url, base2_anchor)
 
-# query the website and return the html to the variable 'page'
-response = urllib2.urlopen(base_url)
-page = response.read()
-response.close()
-
-if debug:
-	print("Now doing a RegEx on the page text")
-
-# Go through the page and find all the "Download Free" links
-links = re.findall(r"<a href=\"(?P<link>.+?pdf)\".+?>Download Free<\/a>", str(page), flags=re.IGNORECASE)
 
 for link in links:
 	print("Looking at {0} now".format(link))
@@ -56,6 +70,11 @@ for link in links:
 		#grab the filename
 		filename = link[last_pos+1:]
 
+		# Check to see if there's a ? in the filename, trim it all off if there is
+		last_pos = filename.rfind("?")
+		if last_pos != -1:
+			filename = filename[:last_pos]
+
 		if debug:
 			print(filename)
 
@@ -64,6 +83,8 @@ for link in links:
 			output_dir = magpi_path
 		elif filename.startswith("Essentials"):
 			output_dir = essen_path
+		elif filename.startswith("Hack"):
+			output_dir = hs_path
 		else:
 			output_dir = other_path
 
