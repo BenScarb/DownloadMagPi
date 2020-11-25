23635960a1759713d8187f8ede5ccbedfa9a9925
Ben S <benscarboro@hotmail.com>
2020-11-25 08:57:17 +0000

    Updated to the new web pages Restructured the code
    Sorry, looks like it will download some of the files again as the names have changed

--- all_issues.py
+++ all_issues.py
@@ -6,19 +6,32 @@ import os
 # Debug setting
 debug = False
 
+# Do you want to check for all back issues as well?
+doBackIssues = True
+
 # Place to put the files (Yes, I developed it on windows!)
 # CHANGE THIS FOR YOUR OWN PATH
 output_dir = "D:\\MagPi\\"
 
+def MagazineLink(page):
+    #<a class="c-issue-actions__link c-link u-text-bold" href="/issues/24/pdf">Download Free PDF</a>
+    linkSearch = '<a class=\"c-issue-actions__link c-link u-text-bold\" href=\"(?P<link>.+?pdf)\">Download Free PDF</a>'
+    return re.findall(linkSearch, str(page), flags=re.IGNORECASE)
+
+def BookLinks(page):
+    #<a class="c-button c-button--secondary c-button--block" href="/books/unity-fps/pdf">Free Download</a>
+    linkRef = '<a class="c-button c-button--secondary c-button--block" href="(?P<link>.+?)">Free Download</a>'
+    return re.findall(linkRef, str(page), flags=re.IGNORECASE)
+
 # URLs for all the Raspberry Pi Foundation Free magazines and books
 # With a locaiton and template filename
 urlList = []
-urlList.append(('https://magpi.raspberrypi.org/issues', os.path.join(output_dir, "Magazines", "MagPi<NUMB>.pdf")))
-urlList.append(('https://magpi.raspberrypi.org/books', os.path.join(output_dir, "Books", "<BOOK>")))
-urlList.append(('https://hackspace.raspberrypi.org/issues/', os.path.join(output_dir, "HackSpace", "HackSpaceMagazine<NUMB>.pdf")))
-urlList.append(('https://wireframe.raspberrypi.org/issues/', os.path.join(output_dir, "WireFrame", "Wireframe<NUMB>.pdf")))
-urlList.append(('https://helloworld.raspberrypi.org/issues', os.path.join(output_dir, "HelloWorld", "HelloWorld_<NUMB>.pdf")))
-urlList.append(('https://wireframe.raspberrypi.org/books/', os.path.join(output_dir, "Wireframe_Books", "<BOOK>")))
+urlList.append(('https://magpi.raspberrypi.org/issues', os.path.join(output_dir, "MagPi", "MagPi<NUMB>.pdf"), MagazineLink))
+urlList.append(('https://hackspace.raspberrypi.org/issues/', os.path.join(output_dir, "HackSpace", "HackSpaceMagazine<NUMB>.pdf"), MagazineLink))
+urlList.append(('https://wireframe.raspberrypi.org/issues/', os.path.join(output_dir, "WireFrame", "Wireframe<NUMB>.pdf"), MagazineLink))
+urlList.append(('https://helloworld.raspberrypi.org/issues', os.path.join(output_dir, "HelloWorld", "HelloWorld_<NUMB>.pdf"), MagazineLink))
+urlList.append(('https://magpi.raspberrypi.org/books', os.path.join(output_dir, "Books", "<BOOK>"), BookLinks))
+urlList.append(('https://wireframe.raspberrypi.org/books/', os.path.join(output_dir, "Wireframe_Books", "<BOOK>"), BookLinks))
 
 fileNo = 1
 
@@ -41,74 +54,67 @@ def GetPage(url):
 
     return page
 
-def ProcessFirstLinks(page):
-    #<a class="c-issue-actions__link c-link u-text-bold" href="/issues/24/pdf">Download Free PDF</a>
-    #<a class="c-button c-button--secondary u-mb-x2" href="/issues/23/pdf">Free Download</a>
-    firstSearch = '<a class=\"c-issue-actions__link c-link u-text-bold\" href=\"(?P<link>.+?pdf)\">Download Free PDF</a>'
-    secondSearch = '<a class=\"c-button c-button--secondary u-mb-x2\" href=\"(?P<link>.+?pdf)\">Free Download</a>'
-    ret = []
-    ret += re.findall(firstSearch, str(page), flags=re.IGNORECASE)
-    ret += re.findall(secondSearch, str(page), flags=re.IGNORECASE)
-    return ret
+def CheckAndDownloadFile(url, outfile):
+    # Check to make sure the file doesn't already exist, don't bother getting
+    # the linked page if it exists, less load on severs.
+    if not os.path.isfile(outFile):
+        # Get the download page
+        page = GetPage(url)
+        # Find the iFrame with the actual download link
+        #<iframe src="https://magazines-attachments.raspberrypi.org/issues/full_pdfs/000/000/462/original/MagPi99.pdf?1603959693" class="u-hidden"></iframe>
+        linkRegEx = "<iframe src=\"(?P<link>.+?)\" class=\"u-hidden\"></iframe>"
+        dllink = re.search(linkRegEx, str(page), flags=re.IGNORECASE)
 
-def ProcessSecondPage(page):
-    #<a class="c-link" download="beginners-guide-2nd-ed.pdf" href="https://magazines-static.raspberrypi.org/books/full_pdfs/000/000/001/original/Beginners_Guide_v2.pdf?1568891625">click here to get your free PDF</a>
-    #<a class="c-link" download="hackspace-magazine-issue-24.pdf" href="https://magazines-static.raspberrypi.org/issues/full_pdfs/000/000/260/original/HackSpaceMagazine24.pdf?1571837861">click here to get your free PDF</a>
-    linkRef = '<a class=\"c-link\" download=\".+?\" href=\"(?P<link>.+?)\">click here to get your free pdf</a>'
-    return re.findall(linkRef, str(page), flags=re.IGNORECASE)
+        if dllink is None:
+            print("Download link not found")
+        else:
+            print("Getting :" + outFile)
+            r = requests.get(dllink.group("link"), allow_redirects=True)
+            open(outFile, 'wb').write(r.content)        
 
 # Make sure the output folders is available
-for (url, template) in urlList:
+for (url, template, _) in urlList:
     dir = os.path.dirname(template)
     if not os.path.exists(dir):
         print("Creating '{0}' now".format(dir))
         os.makedirs(dir)
 
-# create a base links
-links = []
 # Get the links from all the pages, using the RegEx supplied
-for (url, template) in urlList:
+for (url, template, processor) in urlList:
     print("URL: " + url + " - Template:" + template)
     # Get the primary page, with all the links
     page = GetPage(url)
-    # Process the page, get the links
-    secondPages = ProcessFirstLinks(page)
-    # Get the URL parts we need
-    urlParts = urlparse(url)
-    prependLink = urlParts.scheme + '://'+ urlParts.netloc
-    # Go through the links
-    for primeLink in secondPages:
-        # Set up the var for processing
-        page = ""
-        outFile = ""
+    # Process the page, get the first link found
+    mainLinks = processor(page)
 
-        # Check to see if there's a number in the URL
-        fileParts = primeLink.split('/')
-        if fileParts[2].isnumeric():
-            # Generate the output filename from the template
-            outFile = template.replace("<NUMB>", "{:02d}".format(int(fileParts[2])))
-            # Check to make sure the file doesn't already exist, don't bother getting
-            # the linked page if it exists, less load on severs.
-            if not os.path.isfile(outFile):
-                page = GetPage(prependLink + primeLink)
-        else:
-            # No number, so get the file name from the link (later)
-            page = GetPage(prependLink + primeLink)
+    if len(mainLinks) == 0:
+        print("No main link(s) found")
+    else:
+        for link in mainLinks:            
+            # Get the URL parts we need
+            urlParts = urlparse(url)
+            prependLink = urlParts.scheme + '://'+ urlParts.netloc
+            # Check to see if there's a number in the URL (at a certain position)
+            fileParts = link.split('/')
+            if fileParts[2].isnumeric():
+                # Generate the output filename from the template
+                outFile = template.replace("<NUMB>", "{:02d}".format(int(fileParts[2])))
+                # Check for the link and download
+                CheckAndDownloadFile(prependLink + link + "/download", outFile)
 
-        # If there's not page to process, skip it
-        if not page == "":
-            # Get the actual download link
-            downloadLink = ProcessSecondPage(page)
-            # If no name has been supplied
-            if outFile == "":
-                # Get the on disk name from the URL
-                fileParts = downloadLink[0].split('/')
-                outFile = template.replace("<BOOK>", fileParts[-1].split('?')[0])
-
-            # Check to see if the file exists already (mainly for the books section)
-            if not os.path.isfile(outFile):
-                print("Getting :" + outFile)
-                r = requests.get(downloadLink[0], allow_redirects=True)
-                open(outFile, 'wb').write(r.content)
+                if doBackIssues:
+                    # Turn the main link into a template
+                    templateLink = (prependLink + link + "/download").replace("/" + fileParts[2] + "/", "/<NUMB>/")
+                    # Go from the beginning to one before the last issue.
+                    for issueNumb in range(1, int(fileParts[2])):
+                        # Generate the output filename from the template
+                        outFile = template.replace("<NUMB>", "{:02d}".format(issueNumb))
+                        # Check for the link and download
+                        CheckAndDownloadFile(templateLink.replace("<NUMB>", str(issueNumb)), outFile)
+            else:
+                # Do a book template replace
+                outFile = template.replace("<BOOK>", fileParts[2] + ".pdf")
+                # Generate the download link and pass it off
+                CheckAndDownloadFile(prependLink + link + "/download", outFile)
 
 print("Done...")
